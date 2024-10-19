c1d9604ad30d0e34c9dfae50cb99ebf41fbd8ebb
Shinshu-University <154774167+Shinshu-University@users.noreply.github.com>
2024-10-19 11:04:37 +0900

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,34 +1,36 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
     if (req.method === 'POST') {
-      const url = 'https://hackingdemo.microcms.io/api/v1/loginform';
-      const headers = {
-        'Content-Type': 'application/json',
-        'X-MICROCMS-API-KEY': 'bVsOaxRIkuEeMt1kYgEyt6ogJfkpwmIKlkv2'
-      };
-      let pwd = '';
-      async function fetchData() {
-        try {
-          const response = await fetch(url, {
-            method: 'GET',
-            headers: headers
-          });
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          const data = await response.json().password;
-          pwd = data;
-        } catch (error) {
-          console.error('Fetch error:', error);
+        const url = 'https://hackingdemo.microcms.io/api/v1/loginform';
+        const headers = {
+            'Content-Type': 'application/json',
+            'X-MICROCMS-API-KEY': 'bVsOaxRIkuEeMt1kYgEyt6ogJfkpwmIKlkv2'
+        };
+        let pwd = '';
+
+        async function fetchData() {
+            try {
+                const response = await fetch(url, {
+                    method: 'GET',
+                    headers: headers
+                });
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                pwd = data.password; // ここを修正
+            } catch (error) {
+                console.error('Fetch error:', error);
+            }
         }
-      }
-      fetchData();
+
+        await fetchData(); // fetchDataの呼び出しをawaitで待つ
 
         const { input } = req.body;
-      if (input = pwd) {
-        res.status(200).end('Authentication success');
-      } else {
-        res.status(403).end('Authentication failed');
-      }
+        if (input === pwd) { // 比較演算子を修正
+            res.status(200).end('Authentication success');
+        } else {
+            res.status(403).end('Authentication failed');
+        }
     } else {
         res.setHeader('Allow', ['POST']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
